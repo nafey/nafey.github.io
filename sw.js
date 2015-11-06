4f469587984e88e082ce0bef4923494ab078b2b9
meeya <ynafey@gmail.com>
2015-11-06 23:06:47 +0530

    7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 this.addEventListener('fetch', function(event) {
 	event.respondWith(
-		new Response('Hello world from service worker');
+		new Response('Hello world from service worker')
 	);
 });
